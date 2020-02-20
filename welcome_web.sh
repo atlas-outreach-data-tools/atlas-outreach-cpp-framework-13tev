3541f61526fef2550618463845bb7bef17bbcd29
Leonid Serkin <57137451+lserkin@users.noreply.github.com>
2020-02-20 03:36:22 +0100

    Update welcome_web.sh

--- welcome_web.sh
+++ welcome_web.sh
@@ -4,17 +4,5 @@
 #                                                     #   
 #######################################################
 
-choice=1
-
-# names of 12 analyses
-analysisCollection=("WBosonAnalysis" "ZBosonAnalysis" "TTbarAnalysis" "SingleTopAnalysis" "WZDiBosonAnalysis" "ZZDiBosonAnalysis" "HWWAnalysis" "HZZAnalysis" "ZTauTauAnalysis" "HyyAnalysis" "SUSYAnalysis" "ZPrimeBoostedAnalysis")
-
-# creating the output directory for the Analyses codes
-if (( $choice == 1 )) ; then
-  for i in {0..11} ; do
-    analysisName=${analysisCollection[$i]}
-    mkdir Analysis/${analysisName}/Output_${analysisName}
-  done
-
-  mkdir Plotting/histograms
-
+mkdir Analysis/HyyAnalysis/Output_HyyAnalysis/
+mkdir Plotting/histograms
