eb6695cf1037d95931767bb06f567c91521df4da
Leonid Serkin <lserkin@cern.ch>
2020-02-20 03:19:48 +0100

    update

--- welcome_web.sh
+++ welcome_web.sh
@@ -4,7 +4,6 @@
 #                                                     #   
 #######################################################
 
-echo 'Welcome to ATLAS 13 TeV Open Data C++ framework!!!'
 choice=1
 
 # names of 12 analyses
@@ -14,30 +13,8 @@ analysisCollection=("WBosonAnalysis" "ZBosonAnalysis" "TTbarAnalysis" "SingleTop
 if (( $choice == 1 )) ; then
   for i in {0..11} ; do
     analysisName=${analysisCollection[$i]}
-    echo "Creating the directory: Analysis/${analysisName}/Output_${analysisName}"
     mkdir Analysis/${analysisName}/Output_${analysisName}
   done
 
-  echo "Creating the directory for the Plotting code: Plotting/histograms"
   mkdir Plotting/histograms
-
-# removing everything within directories
-elif  (( $choice == 0 )) ; then
-  for i in {0..11} ; do
-    analysisName=${analysisCollection[$i]}
-    echo "Removing the directory: Analysis/${analysisName}/Output_${analysisName}"
-    rm -rf Analysis/${analysisName}/Output_${analysisName}
-  done
-
-  echo "Removing the directory for the Plotting code: Plotting/histograms"
-  rm -rf Plotting/histograms
-
-else
-     echo "Invalid choice!"
-fi
-
-########################
-
-
-
 
