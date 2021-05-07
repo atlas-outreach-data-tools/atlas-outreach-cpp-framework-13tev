7d20c7c40abbed8fa52276c1432ec30acb4d6c72
ArturoS <arturos@cern.ch>
2021-05-07 14:07:43 +0200

    Changing URL and adding mirrors for the datasets --> ticket MYATLAS-78
    Changing URL and adding mirrors for the datasets --> ticket MYATLAS-78

--- Analysis/HyyAnalysis/main_HyyAnalysis.C
+++ Analysis/HyyAnalysis/main_HyyAnalysis.C
@@ -13,7 +13,16 @@ void main_HyyAnalysis(int proof = 0, int option= 0)
   //TString path = "/eos/project/a/atlas-outreach/projects/open-data/OpenDataTuples/renamedLargeRJets/GamGam/";
   
   /* The URL to the ATLAS Open Data website repository */
-  TString path = "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/GamGam/";
+  //TString path = "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/GamGam/";
+
+  /* The URL to the CERN Open Data portal repository */
+  TString path = "http://opendata.cern.ch/eos/opendata/atlas/OutreachDatasets/2020-01-22/GamGam/"
+  
+  /* The XROOTD path to the CERN Open Data portal repository */
+  //TString path = "root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/GamGam/"
+  
+  /* An external mirror */
+  //TString path = "http://www.universidad.ch/ATLAS/outreach/open-data-2020/GamGam/"
 
   //***************************************************************************************************//
   // We run only over data and Higgs MC
