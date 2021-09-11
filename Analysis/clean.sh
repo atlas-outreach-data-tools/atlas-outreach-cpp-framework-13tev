402cf2afda53e630f6fe5580e53128f39b19daf2
ArturoS <arturos@cern.ch>
2021-09-11 19:40:05 +0200

    cleaning all the temporary files in each of the analysis folders

--- Analysis/clean.sh
+++ Analysis/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-## generic cleaning of unnecessary files after linking and compilation
-rm -rf *_ACLiC_*
-rm -rf *.d
-rm -rf *.pcm
-rm -rf *.so
+## generic cleaning of temporary files after linking and compilation
+rm -rf *Analysis/*_ACLiC_*
+rm -rf *Analysis/*.d
+rm -rf *Analysis/*.pcm
+rm -rf *Analysis/*.so
 rm -rf *Analysis/Output_*Analysis/* 
