2f9c38278b13450c10fc5bbb393177add22ca6f4
garciarm <miguel.angel.garcia.ruiz@cern.ch>
2025-03-13 02:04:01 +0100

    Remove third release folder in order to organize better the new analyses

--- third-release-lumi-36/Analysis/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## generic cleaning of temporary files after linking and compilation
-rm -rf *Analysis/*_ACLiC_*
-rm -rf *Analysis/*.d
-rm -rf *Analysis/*.pcm
-rm -rf *Analysis/*.so
-rm -rf *Analysis/Output_*Analysis/* 
