a7caf962940d5e8e0ee909a502bd7217998680c8
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2026-02-11 17:01:48 +0100

    Deleting welcole web file which only included Hyy analysis

--- welcome_web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#######################################################
-#                                                     # 
-# Welcome to ATLAS 13 TeV Open Data C++ framework !!! #
-#                                                     #   
-#######################################################
-
-mkdir Analysis/HyyAnalysis/Output_HyyAnalysis/
-mkdir Plotting/histograms
